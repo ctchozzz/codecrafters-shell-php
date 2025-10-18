d7dccd6d2527335cdba8a428a5d1399c3ce608af
ChoChihTun <chochihtun@gmail.com>
2025-10-18 13:42:58 +0800

    feat: echo

--- app/main.php
+++ app/main.php
@@ -7,12 +7,16 @@ while (!$should_exit) {
 
     // Wait for user input
     $input = trim(fgets(STDIN));
-    $input_array = explode(" ", $input);
+    $input_array = explode(" ", $input, 2);
     $cmd = $input_array[0];
     switch ($cmd) {
         case "exit":
             $arg = $input_array[1];
             $should_exit = true;
+            break;
+        case "echo":
+            $arg = $input_array[1];
+            fwrite(stream: STDOUT, data: $arg . "\n");
             break;
         default:
             fwrite(stream: STDOUT, data: $cmd . ": command not found\n");
