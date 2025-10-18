2dc486a77a3752cddb6cc815b9fbabfd68ded853
ChoChihTun <chochihtun@gmail.com>
2025-10-18 13:48:03 +0800

    feat: type cmd

--- app/main.php
+++ app/main.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL);
 
+$supported_cmd = array("exit", "echo", "type");
 $should_exit = false;
 while (!$should_exit) {
     fwrite(STDOUT, "$ ");
@@ -17,6 +18,14 @@ while (!$should_exit) {
         case "echo":
             $arg = $input_array[1];
             fwrite(stream: STDOUT, data: $arg . "\n");
+            break;
+        case "type":
+            $arg = $input_array[1];
+            if (in_array($arg, $supported_cmd)) {
+                fwrite(STDOUT, data: $arg . " is a shell builtin\n");
+            } else {
+                fwrite(stream: STDOUT, data: $arg . ": not found\n");
+            }
             break;
         default:
             fwrite(stream: STDOUT, data: $cmd . ": command not found\n");
