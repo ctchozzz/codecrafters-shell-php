0c52d36efc4940b07b859e93c0c16b4416ad321a
ChoChihTun <chochihtun@gmail.com>
2025-10-18 13:39:23 +0800

    feat: exit cmd

--- app/main.php
+++ app/main.php
@@ -1,11 +1,22 @@
 <?php
 error_reporting(E_ALL);
 
-while (true) {
+$should_exit = false;
+while (!$should_exit) {
     fwrite(STDOUT, "$ ");
-    
+
     // Wait for user input
     $input = trim(fgets(STDIN));
-    fwrite(stream: STDOUT, data: $input.": command not found\n");
+    $input_array = explode(" ", $input);
+    $cmd = $input_array[0];
+    switch ($cmd) {
+        case "exit":
+            $arg = $input_array[1];
+            fwrite(stream: STDOUT, data: "exit " . $arg);
+            $should_exit = true;
+            break;
+        default:
+            fwrite(stream: STDOUT, data: $cmd . ": command not found\n");
+    }
 }
 
