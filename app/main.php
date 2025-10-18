cb26272f7e5d1c2d69fc30ae595fea9dfa486925
ChoChihTun <chochihtun@gmail.com>
2025-10-18 13:30:09 +0800

    feat: chain of repl

--- app/main.php
+++ app/main.php
@@ -1,10 +1,11 @@
 <?php
 error_reporting(E_ALL);
 
-// Uncomment this block to pass the first stage
-fwrite(STDOUT, "$ ");
-
-// Wait for user input
-$input = trim(fgets(STDIN));
-fwrite(stream: STDOUT, data: $input.": command not found");
+while (true) {
+    fwrite(STDOUT, "$ ");
+    
+    // Wait for user input
+    $input = trim(fgets(STDIN));
+    fwrite(stream: STDOUT, data: $input.": command not found\n");
+}
 
