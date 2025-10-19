c51a2767ac2eebea961b720fc4e875722fc85bbd
ChoChihTun <chochihtun@gmail.com>
2025-10-19 17:21:27 +0800

    debug: nits

--- app/main.php
+++ app/main.php
@@ -46,7 +46,7 @@ while (!$should_exit) {
 
             // exec exists
             print (escapeshellcmd($cmd . " " . $input_array[1] . "\n"));
-            $output = shell_exec(escapeshellcmd($cmd) . " " . $input_array[1]);
+            $output = shell_exec(escapeshellcmd($cmd . " " . $input_array[1]));
             fwrite(stream: STDOUT, data: $output);
     }
 }
