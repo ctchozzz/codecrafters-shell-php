9903e766f76e1f5ba1927421f9427e155a331d07
ChoChihTun <chochihtun@gmail.com>
2025-10-19 17:43:48 +0800

    debug: nits

--- app/main.php
+++ app/main.php
@@ -45,7 +45,7 @@ while (!$should_exit) {
             }
 
             // exec exists
-            $output = shell_exec("\"" . $cmd_path . "\" " . $input_array[1]);
+            $output = shell_exec("'" . $cmd_path . "' " . $input_array[1]);
             fwrite(stream: STDOUT, data: $output);
     }
 }
