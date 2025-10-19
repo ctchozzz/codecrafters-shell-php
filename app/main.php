2de483bbcc2740ed2d2aa665c7f41eb56e4317b6
ChoChihTun <chochihtun@gmail.com>
2025-10-19 18:05:00 +0800

    debug: nits

--- app/main.php
+++ app/main.php
@@ -46,9 +46,9 @@ while (!$should_exit) {
 
             // exec exists
             $closing_quote = "'";
-            if (str_contains($cmd_path, "\'")) {
-                $closing_quote = "\"";
-            }
+            // if (str_contains($cmd_path, "\'")) {
+            //     $closing_quote = "\"";
+            // }
             $new_cmd = $closing_quote . $cmd_path . $closing_quote . " " . $input_array[1];
             $output = shell_exec($new_cmd);
             fwrite(stream: STDOUT, data: $output);
